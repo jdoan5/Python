62acdddfd092026581055870abf8fd9dca6c3a65
John Doan <30330279+jdoan5@users.noreply.github.com>
2023-06-12 11:59:10 -0400

    tip_calculator

--- Python_projects/tip_calculator.py
+++ Python_projects/tip_calculator.py
@@ -6,14 +6,14 @@
 print("Tip Calculator")
 bill = float(input("Please enter the total bill: \n$"))
 total_people = int(input("How many people to split the bill: \n"))
-percentage_tip = float(input("What percentage tip would you like to give? 10%, 12%, or 15%\n"))
+percentage_tip = float(input("What percentage tip would you like to give?\n"))
 
 # Calculate
 tip_amount = percentage_tip / 100
 total_tip_amount = tip_amount * bill
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / total_people
-final_result = bill_per_person
+final_result = round(bill_per_person,2)
 
 # Print result
 print(f"The bill costs ${bill} plus {percentage_tip}% tip gratuity and there are {total_people} people.")
