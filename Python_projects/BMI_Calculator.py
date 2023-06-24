62e5b1f87c6421451cc6aff40cfe3ab727a14c62
John Doan <30330279+jdoan5@users.noreply.github.com>
2023-06-24 08:15:23 -0400

    Update BMI_Calculator.py

--- Python_projects/BMI_Calculator.py
+++ Python_projects/BMI_Calculator.py
@@ -1,11 +1,20 @@
 # A program to calculate the Body Mass Index (BMI)
 
 # User input
-weight = int(input("Enter your weight in kilograms: "))
+""" weight = int(input("Enter your weight in kilograms: "))
 height = float(input("Enter your height in meters: "))
 
 # Calculated the input
 result = weight / (height ** 2)
 
 # Convert the result into integer instead a float result 
-print(int(result))
+print(int(result)) """
+
+def do_the_mess(paramter):
+    paramter[0] += variable
+    return paramter[0]
+
+the_list = [x for x in range(2,3)]
+variable = -1
+do_the_mess(the_list)
+print(the_list[0])
