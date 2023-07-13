e3aa96eb5feeae7b19f94abe701f143b38822e66
John Doan <30330279+jdoan5@users.noreply.github.com>
2023-07-13 17:08:48 -0400

    Python

--- Python_projects/Matching_Game/app.py
+++ Python_projects/Matching_Game/app.py
@@ -9,6 +9,10 @@ display.set_caption('My Game')
 #set the screen size
 screen = display.set_mode((512, 512))
 
+matched = image.load('other_assets/matched.png')
+screen.blit(matched, (0, 0))
+display.flip()
+
 # set the game "true" to continue or "false" to stop
 running = True
 
