7b73c83d9f07b6e81965097e3e943d7a24aeea2f
John Doan <30330279+jdoan5@users.noreply.github.com>
2024-08-17 23:48:20 -0400

    Update city_generator.py

--- Python_projects/city_generator.py
+++ Python_projects/city_generator.py
@@ -1,4 +1,5 @@
 # Create a cityband name generator based on user input
 country = input("Hello, please enter your favorite country: ")
 city = input("Next, what's your favorite city: ")
-print("Your next destination could be " + country + city)
+state = input("Finally, what's your state: ")
+print("Your next destination could be " + country + city + state)
